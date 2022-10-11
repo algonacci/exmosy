9189125d9eeea60466ad3adf58e639f6f047b4a6
algonacci <algonacci1@gmail.com>
2022-10-11 21:18:28 +0700

    add english classifer endpoint

--- app.py
+++ app.py
@@ -1,23 +1,49 @@
 from flask import Flask, render_template, request
-from utils import recognize
+from utils import recognize_indo, recognize_english
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.get("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/indonesian", methods=["GET", "POST"])
+def indonesian():
     if request.method == "POST":
         text = request.form["text"]
+        text = text.strip()
         if text:
-            label, score = recognize(text)
-            return render_template("index.html",
-                                    text=text,
-                                    label=label.capitalize(),
-                                    score=round(score*100))
+            label, score = recognize_indo(text)
+            return render_template("indonesia.html",
+                                   text=text,
+                                   label=label.capitalize(),
+                                   score=round(score*100))
         else:
-            return render_template("index.html",
-            alert="Please input some text to be recognized!")    
+            return render_template("indonesia.html",
+                                   alert="Please input some text to be recognized!")
     else:
-        return render_template("index.html")
+        return render_template("indonesia.html")
+
+
+@app.route("/english", methods=["GET", "POST"])
+def english():
+    if request.method == "POST":
+        text = request.form["text"]
+        text = text.strip()
+        if text:
+            label, score = recognize_english(text)
+            return render_template("english.html",
+                                   text=text,
+                                   label=label.capitalize(),
+                                   score=round(score*100))
+        else:
+            return render_template("english.html",
+                                   alert="Please input some text to be recognized!")
+    else:
+        return render_template("english.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,14 +1,34 @@
-from transformers import pipeline
-pretrained_name = "StevenLimcorn/indonesian-roberta-base-emotion-classifier"
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
-nlp = pipeline(
+pretrained_indo = "StevenLimcorn/indonesian-roberta-base-emotion-classifier"
+
+pretrained_english = AutoModelForSequenceClassification.from_pretrained(
+    "bergum/xtremedistil-l6-h384-go-emotion")
+english_tokenizer = AutoTokenizer.from_pretrained(
+    "bergum/xtremedistil-l6-h384-go-emotion")
+
+nlp_indo = pipeline(
     "sentiment-analysis",
-    model=pretrained_name,
-    tokenizer=pretrained_name
+    model=pretrained_indo,
+    tokenizer=pretrained_indo
 )
 
-def recognize(text):
-    processed = nlp(text)
+nlp_english = pipeline(
+    "text-classification",
+    model=pretrained_english,
+    tokenizer=english_tokenizer
+)
+
+
+def recognize_indo(text):
+    processed = nlp_indo(text)
+    label = processed[0].get("label")
+    score = round(processed[0].get("score"), 2)
+    return label, score
+
+
+def recognize_english(text):
+    processed = nlp_english(text)
     label = processed[0].get("label")
     score = round(processed[0].get("score"), 2)
     return label, score
